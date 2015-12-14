b0905af741871ec5ae33dff3ee238e31fa3a8a6c
ofreax <bertrand.gouny@gmail.com>
2015-12-14 22:33:16 +0100

    sed with follow-symlinks

--- image/service/keepalived/container-start.sh
+++ image/service/keepalived/container-start.sh
@@ -10,14 +10,14 @@ if [ ! -e "$FIRST_START_DONE" ]; then
   #
   # bootstrap config
   #
-  sed -i "s|{{ keepalived_interface }}|$KEEPALIVED_INTERFACE|g" /etc/keepalived/keepalived.conf
-  sed -i "s|{{ keepalived_priority }}|$KEEPALIVED_PRIORITY|g" /etc/keepalived/keepalived.conf
-  sed -i "s|{{ keepalived_password }}|$KEEPALIVED_PASSWORD|g" /etc/keepalived/keepalived.conf
+  sed -i --follow-symlinks --follow-symlinks "s|{{ keepalived_interface }}|$KEEPALIVED_INTERFACE|g" /etc/keepalived/keepalived.conf
+  sed -i --follow-symlinks --follow-symlinks "s|{{ keepalived_priority }}|$KEEPALIVED_PRIORITY|g" /etc/keepalived/keepalived.conf
+  sed -i --follow-symlinks --follow-symlinks "s|{{ keepalived_password }}|$KEEPALIVED_PASSWORD|g" /etc/keepalived/keepalived.conf
 
   if [ -n "$KEEPALIVED_NOTIFY" ]; then
-    sed -i "s|{{ keepalived_notify }}|notify \"$KEEPALIVED_NOTIFY\"|g" /etc/keepalived/keepalived.conf
+    sed -i --follow-symlinks --follow-symlinks "s|{{ keepalived_notify }}|notify \"$KEEPALIVED_NOTIFY\"|g" /etc/keepalived/keepalived.conf
   else
-    sed -i "/{{ keepalived_notify }}/d" /etc/keepalived/keepalived.conf
+    sed -i --follow-symlinks --follow-symlinks "/{{ keepalived_notify }}/d" /etc/keepalived/keepalived.conf
   fi
 
   # unicast peers
@@ -27,13 +27,13 @@ if [ ! -e "$FIRST_START_DONE" ]; then
     # it's just a peer
     # stored in a variable
     if [ -n "${!peer}" ]; then
-      sed -i "s|{{ keepalived_unicast_peers }}|${!peer}\n    {{ keepalived_unicast_peers }}|g" /etc/keepalived/keepalived.conf
+      sed -i --follow-symlinks --follow-symlinks "s|{{ keepalived_unicast_peers }}|${!peer}\n    {{ keepalived_unicast_peers }}|g" /etc/keepalived/keepalived.conf
     # directly
     else
-      sed -i "s|{{ keepalived_unicast_peers }}|${peer}\n    {{ keepalived_unicast_peers }}|g" /etc/keepalived/keepalived.conf
+      sed -i --follow-symlinks --follow-symlinks "s|{{ keepalived_unicast_peers }}|${peer}\n    {{ keepalived_unicast_peers }}|g" /etc/keepalived/keepalived.conf
     fi
   done
-  sed -i "/{{ keepalived_unicast_peers }}/d" /etc/keepalived/keepalived.conf
+  sed -i --follow-symlinks --follow-symlinks "/{{ keepalived_unicast_peers }}/d" /etc/keepalived/keepalived.conf
 
   # virtual ips
   KEEPALIVED_VIRTUAL_IPS=($KEEPALIVED_VIRTUAL_IPS)
@@ -42,13 +42,13 @@ if [ ! -e "$FIRST_START_DONE" ]; then
     # it's just a peer
     # stored in a variable
     if [ -n "${!vip}" ]; then
-      sed -i "s|{{ keepalived_virtual_ips }}|${!vip}\n    {{ keepalived_virtual_ips }}|g" /etc/keepalived/keepalived.conf
+      sed -i --follow-symlinks --follow-symlinks "s|{{ keepalived_virtual_ips }}|${!vip}\n    {{ keepalived_virtual_ips }}|g" /etc/keepalived/keepalived.conf
     # directly
     else
-      sed -i "s|{{ keepalived_virtual_ips }}|${vip}\n    {{ keepalived_virtual_ips }}|g" /etc/keepalived/keepalived.conf
+      sed -i --follow-symlinks --follow-symlinks "s|{{ keepalived_virtual_ips }}|${vip}\n    {{ keepalived_virtual_ips }}|g" /etc/keepalived/keepalived.conf
     fi
   done
-  sed -i "/{{ keepalived_virtual_ips }}/d" /etc/keepalived/keepalived.conf
+  sed -i --follow-symlinks --follow-symlinks "/{{ keepalived_virtual_ips }}/d" /etc/keepalived/keepalived.conf
 
   touch $FIRST_START_DONE
 fi
