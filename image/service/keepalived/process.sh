6910751d38034f5afdda39f8433067a55e7f33da
Bertrand Gouny <bertrand.gouny@osixia.net>
2017-04-02 16:03:27 +0200

    wait config file + fix ip detection

--- image/service/keepalived/process.sh
+++ image/service/keepalived/process.sh
@@ -4,4 +4,12 @@
 # https://github.com/osixia/docker-light-baseimage/blob/stable/image/tool/log-helper
 log-helper level eq trace && set -x
 
+echo -n "Waiting config file /usr/local/etc/keepalived/keepalived.conf"
+while [ ! -e "/usr/local/etc/keepalived/keepalived.conf" ]
+do
+  echo -n "."
+  sleep
+done
+echo "ok"
+
 exec /usr/local/sbin/keepalived -f /usr/local/etc/keepalived/keepalived.conf --dont-fork --log-console ${KEEPALIVED_COMMAND_LINE_ARGUMENTS}
